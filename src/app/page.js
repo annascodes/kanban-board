22d683e460b2f7a66e5e10825b2272c95e3d2982
annascodes <itsanascode@gmail.com>
2025-11-26 07:47:49 +0500

    third

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="h-svh flex justify-center items-center ">
       <div>
         <h1 className="text-5xl font-extrabold tracking-wider">Kanban.</h1>
-        <button className="btn btn-outline btn-neutral text-xs tracking-widest">
-          <Link href={'/tasks'}>My tasks</Link>
-        </button>
+        <div className="flex items-center gap-3">
+          <button className="btn btn-outline btn-neutral text-xs tracking-widest">
+            <Link href={'/tasks'}>My tasks</Link>
+
+          </button>
+          <button className="btn btn-outline btn-neutral text-xs tracking-widest ">
+            <Link target="_blank" href={'https://github.com/annascodes/kanban-board'} className="flex items-center gap-2">
+              <FaGithub className="text-xl" />
+
+              Source code on GitHub
+            </Link>
+
+          </button>
+        </div>
       </div>
 
     </div>
